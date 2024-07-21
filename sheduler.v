ffc2c7266c83e69171144a1dd308ea06a55bdf7a
kufzuk <vologin.vadim.v@gmail.com>
2024-07-21 19:43:30 +0300

    more or less normal general structure. no any barriers now

--- sheduler.v
+++ sheduler.v
@@ -1,61 +1,66 @@
+`define frame global_tp[9: 4]
+
 module sheduler
 
 // Resolution and refresh rate  
 
 #(
-
+    
 )
     //reploace all the magic numbers by parameters
 (
-    input clock, 
-    input data_frames_in   ...  // same as lower
+    input wire clk, 
+    input wire data_frames_in [1023: 0][15: 0]  // same as lower
 );
 
-    reg [1023: 0] data_frames [ 15: 0];   
-    reg [63: 0] data_frames [15: 0][15: 0];   
+    reg   data_frames    [1023: 0][15: 0];   // better change type of memory later
+//    reg [63: 0] data_frames [15: 0][15: 0];   // one big more than many small
+//    ones
+
+
     // continious instructions
    
     // reg [  63: 0] data_frames [255: 0]; 
     // one frame for 16 instructions
                                         //// ??????????????
                                         //    how to divide into frames
+    reg [ 3: 0]     tp = 4'b0;   // task pointer
+    reg [ 5: 0] if_num = 6'b0;
+//    reg [ 5: 0]  frame = 6'b0;   // frame pointer
 
+    
+    reg [ 1: 0]         fence;   // barrier
+    reg [ 9: 0]     global_tp;   // [3:0] = tp, [9:4] = frame
+    reg [15: 0]     exec_mask;   // 1 -> 0 if the core is ready
+    reg [15: 0]  init_r0_vect;
+    reg [31: 0]  mess_to_core;
 
-
-    reg [15: 0]     core_active_vect;   //active cores
-    reg [ 1: 0]                fence;   // barrier
-
-    reg [ 5: 0]                   tp;   // task pointer
-    reg [ 5: 0]               if_num;
-    reg [15: 0]            exec_mask;   // 1 -> 0 if the core is ready
-
-
-    integer frame;
-    always @(posedge clock) begin
+    always @(posedge clk) begin
         data_frames <= data_frames_in;
+        cores cores (); // exec_mask, r0, mess_to_core, core_read_f
+                            // must change exec mask
 
-        for (frame = 0; frame < ; frame = frame + 1) begin
-            if (if_num == 0) begin
-                // initially must be CF
-                
-                // consider fence as if it works only if it is considered only during
-                // start of frame exec
-                fence  <= data_frames[frame * 256 + 0][7: 6];
-                if_num <= data_frames[frame * 256 + 0][5: 0]; 
-                // OR
-                fence  <= data_frames[frame][0][7: 6];
-                if_num <= data_frames[frame][0][5: 0]; 
+        if (if_num == 0) begin
+            // initially must be CF
+            
+            // consider fence as if it works only if it is considered only during
+            // start of frame exec
+            fence        <= data_frames[frame * 256 + 0][ 7: 6];
+            if_num       <= data_frames[frame * 256 + 0][ 5: 0]; 
+            exec_mask    <= data_frames[frame * 256 + 1][15: 0];
+            init_r0_vect <= data_frames[frame * 256 + 2][15: 0]; // ?? better send it straigtly
 
+            // next write or transmit registers data
+            frame <= frame + 1;
+        else if (core_read_f == 1) begin
 
-                // next write or transmit registers data
-                
-                cores cores (); // here call of cores
-                                // must change exec mask
-            else
-                if_num <= if_num - 1;
+            mess_to_core[15:  0] <= data_frames[global_tp    ];
+            mess_to_core[31: 16] <= data_frames[global_tp + 1]; //delete it, just change gp
+
+            global_tp <= global_tp + 2; 
+            if_num    <= if_num    - 1;
+
                 //instr exec
+        end // end of if
 
-            end // end of if
-
-        end //end of for
-end
+endmodule
