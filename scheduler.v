739081f4a56c98b52972d4b0fe5dc2d4b2d5ff3a
kufzuk <vologin.vadim.v@gmail.com>
2024-07-25 11:25:32 +0300

    seems to be a correctly working version. A lot to add left

--- scheduler.v
+++ scheduler.v
@@ -8,13 +8,15 @@ module scheduler
 (
     input  wire clk,
     input  wire reset,
-    input  wire core_read_f,
+    input  wire core_reading,
     input  wire [1023: 0][15: 0] data_frames_in,
+    input  wire [15: 0]              core_ready,  // may be better reverse it
     input  wire                    prog_loading,
     
-    output  reg [15: 0]            new_act_core,
+    output  reg [15: 0]            new_act_core, // delete
     output  reg [15: 0]            init_r0_vect,  // only new  data, old not considered here
     output  reg [31: 0]            mess_to_core,
+    output  reg [15: 0]        tmp_mess_to_core,
     output  reg [ 7: 0][15: 0]          r0_data,
     output wire                frame_being_sent
 //change for wires where possible
@@ -25,10 +27,20 @@ module scheduler
 
     reg [ 5: 0]    if_num;
     reg [ 9: 0] global_tp;   // [3:0] = tp, [9:4] = frame
-    reg [ 1: 0]     fence;   // barrier
-    reg [15: 0] exec_mask;   // 1 -> 0 if the core is ready
+    reg [ 1: 0]     fence;   // barrieri
+    reg [15: 0] exec_mask;
     integer i;
     integer k;
+
+    /*
+    always @(posedge clk) begin
+        exec_mask = (!core_ready) & exec_mask;
+    end // first always
+*/
+
+
+
+
 
     always @(posedge clk) begin
 
@@ -36,43 +48,50 @@ module scheduler
            if_num    <= 0;
            global_tp <= 0;
            exec_mask <= 0;
-       end else begin 
+        end else begin 
+        
 
         data_frames <= data_frames_in;
             
         if (!prog_loading) begin
+            exec_mask <= (~core_ready) & exec_mask;
 
-        if (if_num == 0) begin
-            // initially must be CF
-            new_act_core     <= data_frames[global_tp + 1];
+            if (if_num == 0) begin
+                // initially must be CF
+                new_act_core     <= data_frames[global_tp + 1];
 
-            if ((exec_mask & new_act_core) == 0) begin // might be mistake. new_act_core not changed yet
-                fence        <= (data_frames[global_tp    ] & 16'b0000000011000000) >> 6;
-                if_num       <= data_frames[global_tp    ] & 16'b0000000000111111; 
-                init_r0_vect <= data_frames[global_tp + 2];
+                if ((exec_mask & data_frames[global_tp + 1]) == 0) begin 
+                    fence        <= (data_frames[global_tp    ] & 16'b0000000011000000) >> 6;
+                    if_num       <= data_frames[global_tp    ] & 16'b0000000000111111; 
+                    init_r0_vect <= data_frames[global_tp + 2];
 
 
-                for (i = 0; i < 16; i = i + 1) begin
-                    r0_data[i] <= data_frames[global_tp + i + 16];
+                    for (i = 0; i < 16; i = i + 1) begin
+                        r0_data[i] <= data_frames[global_tp + i + 16];
+                    end
+
+                    exec_mask    <= exec_mask | data_frames[global_tp + 1]; // ??? or change for new_act_core???
+                    
+                    global_tp <= global_tp + 10'h10;   // === global tp += 10'd16
+                end 
+
+            end else if (core_reading) begin
+
+                /*
+                mess_to_core[15:  0] <= data_frames[global_tp    ];
+                mess_to_core[31: 16] <= data_frames[global_tp + 1]; //delete it, just change gp
+                global_tp <= global_tp + 2; 
+                */ // return later, temporally changed
+                
+                tmp_mess_to_core[15:  0] <= data_frames[global_tp];
+                global_tp <= global_tp + 1; 
+
+                if (global_tp[3: 0] == 4'b1111) begin  // must be 0000, but ZZ   4'b1110 if needed
+                    if_num <= if_num - 1;
                 end
 
-                exec_mask    <= exec_mask | data_frames[global_tp + 1]; // ??? or change for new_act_core???
-                
-                global_tp <= global_tp + 10'h10;   // === global tp += 10'd16
-            end 
-
-            end else if (core_read_f) begin
-
-            mess_to_core[15:  0] <= data_frames[global_tp    ];
-            mess_to_core[31: 16] <= data_frames[global_tp + 1]; //delete it, just change gp
-            global_tp <= global_tp + 2; 
-
-            if (global_tp[3: 0] == 4'b1110) begin  // must be 0000, but ZZ
-                if_num <= if_num - 1;
-            end
-
-        end // end of if
+            end // end of if
         end // prog loading
-            end // of reset    
+        end // of reset    
     end // for always
 endmodule
--- testbench.v
+++ testbench.v
@@ -4,8 +4,9 @@ module testbench;
 
     reg clk;
     reg reset;
-    reg core_read_f;
+    reg core_reading;
     reg [1023: 0][15: 0] data_frames_in;
+    reg [15: 0]              core_ready;
     reg                    prog_loading;
     wire               frame_being_sent;
     integer i; 
@@ -18,7 +19,8 @@ module testbench;
     scheduler scheduler (
                          .clk             (             clk),
                          .reset           (           reset),
-                         .core_read_f     (     core_read_f),
+                         .core_ready      (      core_ready),
+                         .core_reading    (    core_reading),
                          .prog_loading    (    prog_loading),
                          .data_frames_in  (  data_frames_in),
                          .frame_being_sent(frame_being_sent)
@@ -29,8 +31,10 @@ module testbench;
     initial begin
         clk          <= 0;
         reset        <= 1;
-        core_read_f  <= 1;
+        core_ready   <= 16'hffff;
+        core_reading <= 1;
         prog_loading <= 1;
+
     end
 
 	initial begin 
@@ -42,13 +46,60 @@ module testbench;
         data_frames_in[1]  =  16'h000f;
         data_frames_in[2]  =  16'h000f;
 
-        for (i = 3; i < 1024; i = i + 1) begin
+        for (i = 3; i < 64; i = i + 1) begin
             data_frames_in[i] = $random;
         end
+
+        
+
+        data_frames_in[64]  =  16'h0003;
+        data_frames_in[65]  =  16'h00f0; // must wait previous. add changin ready flag
+        data_frames_in[66]  =  16'h00f0;
+
+        for (i = 67; i < 128; i = i + 1) begin
+            data_frames_in[i] = $random;
+        end
+
+
+
+        data_frames_in[128]  =  16'h0007;
+        data_frames_in[129]  =  16'h00f0;
+        data_frames_in[130]  =  16'h00f0;
+
+        for (i = 131; i < 256; i = i + 1) begin
+            data_frames_in[i] = $random;
+        end
+
+        data_frames_in[256]  =  16'h002f;
+        data_frames_in[257]  =  16'h0f00;
+        data_frames_in[258]  =  16'h0f00;
+
+        for (i = 259; i < 1024; i = i + 1) begin
+            data_frames_in[i] = $random;
+        end
+
+
+
         #10;
         prog_loading = 0;
+        core_ready   = 16'hfff0;
 
-    
+        #35;
+
+        #65;
+        core_ready = 16'hff00;
+
+        #60; // 190
+        core_ready = 16'hff0f;
+
+        #450;
+        core_ready = 16'hffff;
+
+        #2;
+        core_ready = 16'hff0f;
+
+
+/*
 
 /*
         data_frames_in[66: 64]  =  48'h000f000f0003;
@@ -63,7 +114,7 @@ module testbench;
         data_frames_in[191: 144] = 768'h909349783518983001342480463695987813001572382692929389525816505418711888217498902051905706357098961069882743753078170660315226012310218614980287216204456170791124167120519739573313858667656760
 */
 //        data_frames_in[1023: 192] = 0;
-        #90;
+        #190;
         $finish;
 	end
 
