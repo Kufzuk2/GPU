503414637cf244b4b95b0407828c0f2c8aa10033
KufzuK <vologin.vv@phystech.edu>
2025-04-17 17:41:09 +0300

    harcoded asm now,  some changes in scheduler logic. Now picture in simulation seems to be correct, but we need to check it on the board

--- fpga_version/gpu.v
+++ fpga_version/gpu.v
@@ -1,9 +1,9 @@
 `define FPGA_MODE
 
 module gpu(
-	input wire clk,  // ASSIGN CLOCK_50
- 	input wire KEY0, // ASSIGN KEY_0
 
+
+    `ifdef FPGA_MODE
     // sram for prog_loading
     input  wire [15: 0]data_input, // ASSIGN SRAM DQ_[15: 0]
     output wire [19: 0]input_addr, // ASSIGN SRAM_ADDR_[9: 0]
@@ -14,7 +14,9 @@ module gpu(
     output wire mem_lbn, // ASSIGN SRAM_LB_N
     output wire mem_ubn, // ASSIGN SRAM_UB_N
     // ????
+    output wire mem_cke, // ASSIGN DRAM_CKE
     
+    `endif   //using sram
 
 
     `ifdef FPGA_MODE
@@ -28,9 +30,11 @@ module gpu(
     output wire [7:0]  blue     , // ASSIGN VGA_B_[7:0]
 
     `endif
-	 output wire mem_cke // ASSIGN DRAM_CKE
 
-	//		input  wire [1024  - 1: 0][15: 0] data_frames_in //// sdram
+	input wire clk,  // ASSIGN CLOCK_50
+ 	input wire KEY0 // ASSIGN KEY_0
+
+	//input  wire [1024  - 1: 0][15: 0] data_frames_in //// sdram
 );
     wire [15 : 0] core_ready;
     wire [15 : 0] read;
@@ -90,29 +94,44 @@ module gpu(
 
 
 
-assign finish = finish_array[ 0] |  finish_array[ 1] |  finish_array[ 2] | finish_array[ 3] | finish_array[ 4] | finish_array[ 5] | finish_array[ 6] | finish_array[ 7] | 
-		finish_array[ 8] |  finish_array[ 9] |  finish_array[10] | finish_array[11] | finish_array[12] | finish_array[13] | finish_array[14] | finish_array[15] ; 
+assign finish = finish_array[ 0] |  
+                finish_array[ 1] |  
+                finish_array[ 2] | 
+                finish_array[ 3] | 
+                finish_array[ 4] | 
+                finish_array[ 5] | 
+                finish_array[ 6] | 
+                finish_array[ 7] |
+                finish_array[ 8] |  
+                finish_array[ 9] |  
+                finish_array[10] | 
+                finish_array[11] | 
+                finish_array[12] | 
+                finish_array[13] | 
+                finish_array[14] | 
+                finish_array[15] ; 
 
 genvar i;
 
 generate
 	for(i = 0; i < 16; i = i + 1) begin: gen_cores
 		gpu_core_1 gpu_core_i ( 
-					.clk(clk), 
-					.reset(reset), 
-			      		.val_ins(val_ins),
-				    	.val_mask_R0(r0_mask_loading),
-					.val_mask_ac(core_mask_loading),   
-				       	.val_R0(r0_loading), 
-				       	.val_data(finish[i]), 
-					.instruction(instruction),
-				       	.addr_shared_memory(addr_in[11 + 12 * i[3:0] : 12 * i[3:0]]), 
-					.mem_dat(data_out[7 + 8 * i[3:0] : 8 *  i[3:0]]),  .mem_dat_st(data_in[7 + 8 * i : 8 * i]),
-					.core_id(i[3:0]),
-				       	.rtr(gpu_core_reading[i[3:0]]),
-				       	.mem_req_ld(read[i[3:0]]), 
-					.mem_req_st(write[i[3:0]]),
-				       	.ready(core_ready[i[3:0]])
+					.clk                (clk                                     ), 
+					.reset              (reset                                   ), 
+			      	.val_ins            (val_ins                                 ),
+				    .val_mask_R0        (r0_mask_loading                         ),
+					.val_mask_ac        (core_mask_loading                       ),   
+				    .val_R0             (r0_loading                              ), 
+				    .val_data           (finish[i]                               ), 
+					.instruction        (instruction                             ),
+				    .addr_shared_memory (addr_in[11 + 12 * i[3:0] : 12 * i[3:0]] ), 
+					.mem_dat            (data_out[7 + 8 * i[3:0] : 8 *  i[3:0]]  ),  
+                    .mem_dat_st         (data_in[7 + 8 * i : 8 * i]              ),
+					.core_id            (i[3:0]                                  ),
+				    .rtr                (gpu_core_reading[i[3:0]]                ),
+				    .mem_req_ld         (read[i[3:0]]                            ), 
+					.mem_req_st         (write[i[3:0]]                           ),
+				    .ready              (core_ready[i[3:0]]                      )
 		);
 	end
 endgenerate
@@ -123,9 +142,9 @@ generate
 					.clock(clk                           ),
 					.reset(reset                         ),
 					.read(read                           ),
-				       	.write(write                         ),
-                     			.bank_n(i[3:0]                       ),
-				       	.addr_in(addr_in                     ),
+				    .write(write                         ),
+                    .bank_n(i[3:0]                       ),
+				    .addr_in(addr_in                     ),
 					.data_in(data_in                     ),
 
 					`ifdef FPGA_MODE
@@ -133,8 +152,8 @@ generate
 					.data_vga(data_vga[7 + 8 * i[3:0] : 8 * i[3:0]]),
 					`endif
 
-                     			.data_out(data_out                   ),
-				       	.finish(finish_array[i]              )
+                     .data_out(data_out                  ),
+				     .finish(finish_array[i]             )
         	);
 
 	end
@@ -160,13 +179,31 @@ vga vga (
 `endif
 
 
-
+/*
 new_ts gpu_scheduler 
-                    ( .clk(clk), .reset(reset),  .instr_loading(val_ins),
-                      .r0_mask_loading(r0_mask_loading),     .core_mask_loading(core_mask_loading),
-                      .r0_loading(r0_loading),             .core_reading(gpu_core_reading), 
-                      .data_input(data_input), .input_addr(input_addr),
-                      .core_ready(core_ready), .mess_to_core(instruction)
+                    ( .clk              (clk              ), 
+                      .reset            (reset            ),
+                      .instr_loading    (val_ins          ),
+                      .r0_mask_loading  (r0_mask_loading  ),
+                      .core_mask_loading(core_mask_loading),
+                      .r0_loading       (r0_loading       ),
+                      .core_reading     (gpu_core_reading ), 
+                      .data_input       (data_input       ),
+                      .input_addr       (input_addr       ),
+                      .core_ready       (core_ready       ),
+                      .mess_to_core     (instruction      )
                     );
+*/
 
+scheduler gpu_scheduler 
+                    ( .clk              (clk              ), 
+                      .reset            (reset            ),
+                      .instr_loading    (val_ins          ),
+                      .r0_mask_loading  (r0_mask_loading  ),
+                      .core_mask_loading(core_mask_loading),
+                      .r0_loading       (r0_loading       ),
+                      .core_reading     (gpu_core_reading ), 
+                      .core_ready       (core_ready       ),
+                      .mess_to_core     (instruction      )
+                    );
 endmodule
